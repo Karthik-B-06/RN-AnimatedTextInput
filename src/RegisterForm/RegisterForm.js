8e479316a3003a8224bab7041b6ebe89e8af2178
Karthik-B-06 <karthik06051996@gmail.com>
2020-03-07 13:48:18 +0530

    added keyboard aware view

--- src/RegisterForm/RegisterForm.js
+++ src/RegisterForm/RegisterForm.js
@@ -3,7 +3,7 @@ import { StyleSheet, View, Text } from 'react-native';
 import { useFormData } from '../helpers/formData';
 import { COLORS, DEVICE } from '../helpers/styledTheme';
 import FormField from './components/FormField';
-
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 export const RegisterForm = (props) => {
   const [
@@ -17,49 +17,51 @@ export const RegisterForm = (props) => {
   });
   return (
     <View style={{ flex: 1, backgroundColor: COLORS.WHITE }}>
-      <View style={RegisterFormStyles.formContainer}>
-        <View style={{
-          margin: 5
-        }}>
-          <FormField
-            handleChange={handleChange}
-            value={values.name}
-            defaultValue={values.name}
-            textInputId='name'
-            label='Name'
-          />
+      <KeyboardAwareScrollView>
+        <View style={RegisterFormStyles.formContainer}>
+          <View style={{
+            margin: 5
+          }}>
+            <FormField
+              handleChange={handleChange}
+              value={values.name}
+              defaultValue={values.name}
+              textInputId='name'
+              label='Name'
+            />
+          </View>
+          <View style={{
+            margin: 5
+          }}>
+            <FormField
+              handleChange={handleChange}
+              value={values.email}
+              defaultValue={values.email}
+              textInputId='email'
+              label='Email'
+              textInputProps={{
+                textContentType: 'emailAddress',
+                autoCapitalize: 'none'
+              }}
+            />
+          </View>
+          <View style={{
+            margin: 5
+          }}>
+            <FormField
+              handleChange={handleChange}
+              value={values.password}
+              defaultValue={values.password}
+              textInputId='password'
+              label='Password'
+              textInputProps={{
+                secureTextEntry: true,
+                textContentType: 'emailAddress'
+              }}
+            />
+          </View>
         </View>
-        <View style={{
-          margin: 5
-        }}>
-          <FormField
-            handleChange={handleChange}
-            value={values.email}
-            defaultValue={values.email}
-            textInputId='email'
-            label='Email'
-            textInputProps={{
-              textContentType: 'emailAddress',
-              autoCapitalize: 'none'
-            }}
-          />
-        </View>
-        <View style={{
-          margin: 5
-        }}>
-          <FormField
-            handleChange={handleChange}
-            value={values.password}
-            defaultValue={values.password}
-            textInputId='password'
-            label='Password'
-            textInputProps={{
-              secureTextEntry: true,
-              textContentType: 'emailAddress'
-            }}
-          />
-        </View>
-      </View>
+      </KeyboardAwareScrollView>
     </View>
   )
 }
